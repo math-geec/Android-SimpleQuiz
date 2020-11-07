1df98aa45678d4743609a61a55a322ca396ad15b
math-geec <victordean087@gmail.com>
2020-11-07 10:46:15 +0000

    Add questions

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -1,20 +1,115 @@
 package com.example.quizapp
 
-object Constants{
-    fun getQuestions(): ArrayList<Question>{
-        val questionList = ArrayList<Question>()
+
+//Create a Constants Object.
+object Constants {
+
+    //    Create a list of questions using the Question data model class.
+//    And add all the flag images to the drawable folder to add it in the list.
+    fun getQuestions(): ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
 
         val que1 = Question(
             1,
             "Which country does this flag belong to?",
-             R.drawable.ic_flag_of_argentina,
+            R.drawable.ic_flag_of_argentina,
             "Australia",
             "Fuji",
             "Kuwait",
             "Argentina",
-            4)
+            4
+        )
 
-        questionList.add(que1)
-        return questionList
+        questionsList.add(que1)
+
+        val que2 = Question(
+            2, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Angola", "Austria",
+            "Australia", "Armenia", 3
+        )
+
+        questionsList.add(que2)
+
+        // 3
+        val que3 = Question(
+            3, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Belarus", "Belize",
+            "Brunei", "Brazil", 4
+        )
+
+        questionsList.add(que3)
+
+        // 4
+        val que4 = Question(
+            4, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Bahamas", "Belgium",
+            "Barbados", "Belize", 2
+        )
+
+        questionsList.add(que4)
+
+        // 5
+        val que5 = Question(
+            5, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Gabon", "France",
+            "Fiji", "Finland", 3
+        )
+
+        questionsList.add(que5)
+
+        // 6
+        val que6 = Question(
+            6, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Germany", "Georgia",
+            "Greece", "none of these", 1
+        )
+
+        questionsList.add(que6)
+
+        // 7
+        val que7 = Question(
+            7, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Dominica", "Egypt",
+            "Denmark", "Ethiopia", 3
+        )
+
+        questionsList.add(que7)
+
+        // 8
+        val que8 = Question(
+            8, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "Ireland", "Iran",
+            "Hungary", "India", 4
+        )
+
+        questionsList.add(que8)
+
+        // 9
+        val que9 = Question(
+            9, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "Australia", "New Zealand",
+            "Tuvalu", "United States of America", 2
+        )
+
+        questionsList.add(que9)
+
+        // 10
+        val que10 = Question(
+            10, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "Kuwait", "Jordan",
+            "Sudan", "Palestine", 1
+        )
+
+        questionsList.add(que10)
+        return questionsList
     }
 }
