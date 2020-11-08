e5b9d01852bc42245a279f80e4a6cdffcd543a87
math-geec <victordean087@gmail.com>
2020-11-08 16:58:05 +0000

    Connect question UI to the model

--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -7,7 +7,7 @@ data class Question (
     val image: Int,
     val optionOne: String,
     val optionTwo: String,
-    val optionOThree: String,
+    val optionThree: String,
     val optionFour: String,
     val correctAnswer: Int
 )
--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
 // Create a QuizQuestions Activity
 class QuizQuestionActivity : AppCompatActivity() {
@@ -14,5 +15,24 @@ class QuizQuestionActivity : AppCompatActivity() {
         // Get the list of question here to show in the UI
         val questionList = Constants.getQuestions()
         Log.i("Questions Size", "${questionList.size}")
+
+        // Setting the question in the UI from the list
+        val currentPosition = 1
+        // Getting the question from the list with the help of current position.
+        val question: Question? = questionList[currentPosition - 1]
+
+        // Setting the current progress in the progressbar using the position of question
+        progressBar.progress = currentPosition
+        // Setting up the progress text
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        // Now set the current question and the options in the UI
+        // convert to a non-nullable type, !! will throw NullPointerException if the question value is null
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
     }
 }
