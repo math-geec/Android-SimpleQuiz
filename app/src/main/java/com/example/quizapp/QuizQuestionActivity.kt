962ca422683c990127dacd17c8e2deb920492132
math-geec <victordean087@gmail.com>
2020-11-14 19:15:51 +0000

    Preparing the UI for the Result Activity

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -19,6 +19,7 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition: Int = 1
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
+    private var mCorrectAnswers: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -110,6 +111,8 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
                     // This is to check if the answer is wrong
                     if (question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }else{
+                        mCorrectAnswers++
                     }
 
                     // This is for correct answer
